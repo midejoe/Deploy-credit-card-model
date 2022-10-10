ea0b2f0453f2625457f94f3c22129100c506cb03
Joseph Ariyo <midejoe@LAPTOP-8974UDSO>
2022-10-10 22:11:26 +0100

    Add initial commit

---
+++ predict.py
@@ -0,0 +1,25 @@
+# Load the model
+
+import pickle
+
+def load(filename):
+    with open(filename, 'rb') as f_in:
+        return pickle.load(f_in)
+
+dv = load('dv.bin')
+model = load('model1.bin')
+
+client = {
+    "reports": 0,
+    "share": 0.001694,
+    "expenditure": 0.12,
+    "owner": "yes"
+}
+
+X = dv.transform([client])
+
+y_pred = model.predict_proba(X)[0, 1]
+
+print("input", client)
+print("credit card probability", y_pred)
+
